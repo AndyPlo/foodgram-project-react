1946bf4a9ebc07070163073ba4622f8af8dd331d
Andrei Plotnikov <avin@mail.ru>
2022-12-24 16:44:54 +0500

    custom pagination class testing

--- backend/foodgram/api/pagination.py
+++ backend/foodgram/api/pagination.py
@@ -1,6 +1,8 @@
+from django.core import paginator
 from rest_framework.pagination import PageNumberPagination
 
 
-class CustomPagination(PageNumberPagination):
+class CustomPaginator(PageNumberPagination):
+    django_paginator_class = paginator.Paginator
     page_query_param = 'page'
-    page_size_query_param = "limit"
+    page_size_query_param = 'limit'
